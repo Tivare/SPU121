a563b638732de1a141614fb87af34e7f83ef28cd
Khylko Andrii <hilko_a@itstep.academy>
2022-06-16 19:43:10 +0300

    add dif function

--- SPU121/Source.cpp
+++ SPU121/Source.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include"sum.h"
+#include"dif.h"
 using namespace std;
 
 int main()
 {
 	cout << "Hello world" << endl;
 	cout << "Sum = "<<Sum(12,23) << endl;
+	cout << "Dif = "<<Dif(12,23) << endl;
 }
